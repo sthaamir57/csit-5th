4e53548ae36ed4a13ddaf15250afc8f90ad5c7aa
Amir <stha.amir57@gmail.com>
2023-01-21 05:18:18 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function daysLeftForExam() {
+    let daysLeft = document.querySelector('.days-left');
+    let countDownDate = new Date("Feb 9, 2020 12:00:00 GMT+0545").getTime();
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    daysLeft.innerHTML = `${days} days for exams`;
+  
+    if (distance < 0) {
+      daysLeft.innerHTML = "Exams Started";
+    }
+  }
+
+  daysLeftForExam();
