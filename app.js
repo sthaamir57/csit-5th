f2be8a7c43570a8c529962e7259f68c386a04464
Amir <stha.amir57@gmail.com>
2023-01-21 05:22:37 +0545

    fixed countdown date

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function daysLeftForExam() {
     let daysLeft = document.querySelector('.days-left');
-    let countDownDate = new Date("Feb 9, 2020 12:00:00 GMT+0545").getTime();
+    let countDownDate = new Date("Feb 9, 2023 12:00:00 GMT+0545").getTime();
     let now = new Date().getTime();
     let distance = countDownDate - now;
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
